67366eedc28911f5dac1c5e9121b02760b7010e1
Bayojid AI <bbostami385@gmail.com>
2026-03-11 00:17:49 +0600

    Update Login.js
    Signed-off-by: Bayojid AI  <bbostami385@gmail.com>

--- Login.js
+++ Login.js
@@ -1,41 +1,59 @@
 import React, { useState } from "react";
-import LoginLogo from "../assets/login_logo.png";
 
 function Login() {
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
-    try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
-      });
-      const data = await res.json();
-      alert(data.msg);
-    } catch (err) {
-      console.error(err);
-      alert("Server error");
-    }
+
+    const res = await fetch("/api/auth/login", {
+
+      method: "POST",
+
+      headers: {
+        "Content-Type": "application/json"
+      },
+
+      body: JSON.stringify({
+        email,
+        password
+      })
+
+    });
+
+    const data = await res.json();
+
+    alert(data.msg);
+
   };
 
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <img src={LoginLogo} alt="Login Logo" width={150} height={150} />
+
+    <div style={{ textAlign: "center" }}>
+
       <h2>Login</h2>
+
       <input
-        type="email"
-        placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
+      type="email"
+      placeholder="Email"
+      onChange={(e)=>setEmail(e.target.value)}
       />
+
+      <br/><br/>
+
       <input
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
+      type="password"
+      placeholder="Password"
+      onChange={(e)=>setPassword(e.target.value)}
       />
+
+      <br/><br/>
+
       <button onClick={handleLogin}>Login</button>
+
     </div>
+
   );
 }
 
