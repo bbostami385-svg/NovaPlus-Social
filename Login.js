a6db74cd33e625f0b9147be402a4d57fa4be3fa5
Bayojid AI <bbostami385@gmail.com>
2026-03-11 15:54:20 +0600

    Update Login.js
    Signed-off-by: Bayojid AI  <bbostami385@gmail.com>

--- Login.js
+++ Login.js
@@ -2,58 +2,34 @@ import React, { useState } from "react";
 
 function Login() {
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleLogin = async () => {
-
-    const res = await fetch("/api/auth/login", {
-
-      method: "POST",
-
-      headers: {
-        "Content-Type": "application/json"
-      },
-
-      body: JSON.stringify({
-        email,
-        password
-      })
-
-    });
-
-    const data = await res.json();
-
-    alert(data.msg);
-
-  };
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
 
   return (
-
-    <div style={{ textAlign: "center" }}>
+    <div style={{textAlign:"center"}}>
 
       <h2>Login</h2>
 
       <input
-      type="email"
-      placeholder="Email"
-      onChange={(e)=>setEmail(e.target.value)}
+        placeholder="Email"
+        value={email}
+        onChange={(e)=>setEmail(e.target.value)}
       />
 
       <br/><br/>
 
       <input
-      type="password"
-      placeholder="Password"
-      onChange={(e)=>setPassword(e.target.value)}
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e)=>setPassword(e.target.value)}
       />
 
       <br/><br/>
 
-      <button onClick={handleLogin}>Login</button>
+      <button>Login</button>
 
     </div>
-
   );
 }
 
